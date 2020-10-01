dcdd511a4be271846b8434f339da4a4d01074a16
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2020-10-01 17:16:53 +0530

    Update PyTesseract.py

--- PyTesseract.py
+++ PyTesseract.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-
-
 import pytesseract
 from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-text = pytesseract.image_to_string(Image.open('example_02.png'))
+text = pytesseract.image_to_string(Image.open('text.png'))
 print(text)
 
 
