677eb03883b5c1fab61dd771dd5dfc063a59dd8e
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2020-10-01 17:11:56 +0530

    Add files via upload

---
+++ coordinate_finder.py
@@ -0,0 +1,14 @@
+import pyautogui
+from time import sleep
+pyautogui.PAUSE = 1
+sleep(5)
+
+#this pyautogui.position() returns the x and y coordinates of the mouse
+x,y=pyautogui.position()
+print(x,y)
+
+
+#pyautogui.moveTo(x,y) is used to move the mouse from any position to the point x,y pyautogui.click() is used to simulate a mouse click over here.
+
+pyautogui.moveTo(x,y)
+pyautogui.click()
